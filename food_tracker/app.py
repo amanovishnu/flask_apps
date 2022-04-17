7e4e632804c7696a268f91d1b38f4f3fd8dee9ca
amanovishnu <geekymano@gmail.com>
2022-04-17 16:38:44 +0530

    initial setup

---
+++ food_tracker/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, g
+import sqlite3
+
+app = Flask(__name__)
+
+def connect_db():
+    sql = sqlite3.connect('C:\\Users\\vishnu.adepu\\Desktop\\sqlite\\food_log.db')
+    sql.row_factory = sql.Row # converts tuples to dictionary
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite3_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/view')
+def view():
+    return render_template('day.html')
+
+@app.route('/food')
+def food():
+    return render_template('add_food.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
