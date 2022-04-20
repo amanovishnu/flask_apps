23c0c0006bf7dc36ab7439f5230347440f7fdaab
amanovishnu <geekymano@gmail.com>
2022-04-20 16:14:52 +0530

    query modified

--- food_tracker/app.py
+++ food_tracker/app.py
@@ -25,10 +25,10 @@ def index():
                         sum(food.carbohydrates) as carbohydrates, 
                         sum(food.fat) as fat , 
                         sum(food.calories) as calories
-                        from food 
-                        join food_date on food.id = food_date.food_id
-                        join log_date on log_date.id = food_date.log_date_id 
-                        group by log_date.entry_date
+                        from log_date 
+                        left join food_date on food_date.log_date_id = log_date.id
+                        left join food on food.id = food_date.food_id
+                        group by log_date.id
                         order by log_date.entry_date desc;''')
     results = cur.fetchall()
 
