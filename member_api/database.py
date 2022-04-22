74df40e0398a39022269640b9a9f10b375b490cf
amanovishnu <geekymano@gmail.com>
2022-04-22 08:50:22 +0530

    added db path using os module

--- member_api/database.py
+++ member_api/database.py
@@ -1,8 +1,12 @@
 from flask import g
 import sqlite3
+import os
+
+db_path = os.path.join(os.getcwd(), 'members.db')
 
 def connect_db():
-    sql = sqlite3.connect('C:\\Users\\vishnu.adepu\\Desktop\\sqlite\\members.db')
+    # sql = sqlite3.connect('C:\\Users\\vishnu.adepu\\Desktop\\sqlite\\members.db')
+    sql = sqlite3.connect(db_path)
     sql.row_factory = sqlite3.Row
     return sql
 
